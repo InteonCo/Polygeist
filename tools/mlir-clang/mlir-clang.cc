002aba1274740110424fe31071a365fef94fe130
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2022-03-22 10:46:59 +0100

    Link fixme to jira ticket

--- tools/mlir-clang/mlir-clang.cc
+++ tools/mlir-clang/mlir-clang.cc
@@ -458,9 +458,9 @@ int main(int argc, char **argv) {
   bool LinkOMP = FOpenMP;
   pm.enableVerifier(EarlyVerifier);
   mlir::OpPassManager &optPM = pm.nest<mlir::FuncOp>();
-  // JLE_QUEL::FIXME
-  // Mem2RegPass is failing at this point
-  // Fix it, then revert condition to be true
+  /// JLE_QUEL::FIXME
+  /// Mem2RegPass is failing at this point - II-199
+  /// Fix it, then revert condition to be true
   if (false) {
     optPM.addPass(mlir::createCSEPass());
     optPM.addPass(mlir::createCanonicalizerPass());
