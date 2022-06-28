09d4cf770c7ff815afcb79dca4819fe36867ed96
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2022-06-28 11:07:54 +0200

    Update sycl tests with type aliases

--- tools/mlir-clang/Test/sycl/constructors.cpp
+++ tools/mlir-clang/Test/sycl/constructors.cpp
@@ -12,16 +12,19 @@
 
 #include <sycl/sycl.hpp>
 
+// CHECK: !sycl_id_2_ = type !sycl.id<2>
+// CHECK: !sycl_item_2_1_ = type !sycl.item<[2, true], (!sycl.item_base<[2, true], (!sycl.range<2>, !sycl.id<2>, !sycl.id<2>)>)>
+
 // CHECK: func @_Z6cons_1v() attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT: %false = arith.constant false
 // CHECK-NEXT: %c0_i8 = arith.constant 0 : i8
-// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl.id<2>>
-// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl.id<2>> to memref<?x!sycl.id<2>>
-// CHECK-NEXT: %2 = "polygeist.memref2pointer"(%0) : (memref<1x!sycl.id<2>>) -> !llvm.ptr<i8>
-// CHECK-NEXT: %3 = "polygeist.typeSize"() {source = !sycl.id<2>} : () -> index
+// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
+// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
+// CHECK-NEXT: %2 = "polygeist.memref2pointer"(%0) : (memref<1x!sycl_id_2_>) -> !llvm.ptr<i8>
+// CHECK-NEXT: %3 = "polygeist.typeSize"() {source = !sycl_id_2_} : () -> index
 // CHECK-NEXT: %4 = arith.index_cast %3 : index to i64
 // CHECK-NEXT: "llvm.intr.memset"(%2, %c0_i8, %4, %false) : (!llvm.ptr<i8>, i8, i64, i1) -> ()
-// CHECK-NEXT: sycl.constructor(%1) {Type = @id} : (memref<?x!sycl.id<2>>) -> ()
+// CHECK-NEXT: sycl.constructor(%1) {Type = @id} : (memref<?x!sycl_id_2_>) -> ()
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -30,9 +33,9 @@
 }
 
 // CHECK: func @_Z6cons_2mm(%arg0: i64, %arg1: i64) attributes {llvm.linkage = #llvm.linkage<external>} {
-// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl.id<2>>
-// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl.id<2>> to memref<?x!sycl.id<2>>
-// CHECK-NEXT: sycl.constructor(%1, %arg0, %arg1) {Type = @id} : (memref<?x!sycl.id<2>>, i64, i64) -> ()
+// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
+// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
+// CHECK-NEXT: sycl.constructor(%1, %arg0, %arg1) {Type = @id} : (memref<?x!sycl_id_2_>, i64, i64) -> ()
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -40,13 +43,13 @@
   auto id = sycl::id<2>{a, b};
 }
 
-// CHECK: func @_Z6cons_3N2cl4sycl4itemILi2ELb1EEE(%arg0: !sycl.item<2, 1>) attributes {llvm.linkage = #llvm.linkage<external>} {
-// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl.id<2>>
-// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl.id<2>> to memref<?x!sycl.id<2>>
-// CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl.item<2, 1>>
-// CHECK-NEXT: %3 = memref.cast %2 : memref<1x!sycl.item<2, 1>> to memref<?x!sycl.item<2, 1>>
-// CHECK-NEXT: affine.store %arg0, %2[0] : memref<1x!sycl.item<2, 1>>
-// CHECK-NEXT: sycl.constructor(%1, %3) {Type = @id} : (memref<?x!sycl.id<2>>, memref<?x!sycl.item<2, 1>>) -> ()
+// CHECK: func @_Z6cons_3N2cl4sycl4itemILi2ELb1EEE(%arg0: !sycl_item_2_1_) attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
+// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
+// CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl_item_2_1_>
+// CHECK-NEXT: %3 = memref.cast %2 : memref<1x!sycl_item_2_1_> to memref<?x!sycl_item_2_1_>
+// CHECK-NEXT: affine.store %arg0, %2[0] : memref<1x!sycl_item_2_1_>
+// CHECK-NEXT: sycl.constructor(%1, %3) {Type = @id} : (memref<?x!sycl_id_2_>, memref<?x!sycl_item_2_1_>) -> ()
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -54,13 +57,13 @@
   auto id = sycl::id<2>{val};
 }
 
-// CHECK: func @_Z6cons_4N2cl4sycl2idILi2EEE(%arg0: !sycl.id<2>) attributes {llvm.linkage = #llvm.linkage<external>} {
-// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl.id<2>>
-// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl.id<2>> to memref<?x!sycl.id<2>>
-// CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl.id<2>>
-// CHECK-NEXT: %3 = memref.cast %2 : memref<1x!sycl.id<2>> to memref<?x!sycl.id<2>>
-// CHECK-NEXT: affine.store %arg0, %2[0] : memref<1x!sycl.id<2>>
-// CHECK-NEXT: sycl.constructor(%1, %3) {Type = @id} : (memref<?x!sycl.id<2>>, memref<?x!sycl.id<2>>) -> ()
+// CHECK: func @_Z6cons_4N2cl4sycl2idILi2EEE(%arg0: !sycl_id_2_) attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
+// CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
+// CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl_id_2_>
+// CHECK-NEXT: %3 = memref.cast %2 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
+// CHECK-NEXT: affine.store %arg0, %2[0] : memref<1x!sycl_id_2_>
+// CHECK-NEXT: sycl.constructor(%1, %3) {Type = @id} : (memref<?x!sycl_id_2_>, memref<?x!sycl_id_2_>) -> ()
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
